5472b3485d78b59f4fae55b0406cfa6df6b4ee70
jasery <jaserydev@163.com>
2019-05-30 20:41:26 +0800

    【开发】调整云函数环境

--- cloudfunctions/clearSettle/index.js
+++ cloudfunctions/clearSettle/index.js
@@ -1,7 +1,9 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({
+  env: 'test-1e22ad'
+})
 const db = cloud.database();
 const _ = db.command;
 // 云函数入口函数
--- cloudfunctions/confirmSettle/index.js
+++ cloudfunctions/confirmSettle/index.js
@@ -1,55 +1,44 @@
 // 云函数入口文件
 const cloud = require('wx-server-sdk')
 
-cloud.init()
+cloud.init({
+  env: 'test-1e22ad'
+})
 const db = cloud.database();
 const _ = db.command;
 // 云函数入口函数
 exports.main = async (event, context) => {
-  return db.collection('bills').get()
   const wxContext = cloud.getWXContext()
 
   let openId = wxContext.OPENID;
   let groupId = event.groupId;
-  console.log('groupId', groupId)
-  db.collection('group')
-    .doc(groupId)
+  let settleInfos = await db.collection('settle')
+    .where({
+      groupId
+    })
     .get()
-    .then(res => {
-      console.log('group Info-')
-      console.log(res)
-    })
-    .catch(err => {
-      console.log('find group err')
-      console.log(err)
-    })
-    // console.log(groupInfo)
-  // let settleInfos = await db.collection('settle')
-  //   .where({
-  //     groupId
-  //   })
-  //   .get()
-  // console.log('settle res')
-  // console.log(settleInfos)
-  // let settleInfo = settleInfos[0];
-  // if (!settleInfo) {
-  //   return {
-  //     code: 0,
-  //     msg: 'no settle info'
-  //   }
-  // }
-  // if (settleInfo.confirmMembers.indexOf(openId) < 0) {
-  //   await db.collection('group')
-  //     .doc(groupId)
-  //     .update({
-  //       data: {
-  //         confirmMembers: _.push(openId)
-  //       }
-  //     });
-  // }
+  console.log('settle res')
+  console.log(settleInfos)
+  let settleInfo = settleInfos.data[0];
+  if (!settleInfo) {
+    return {
+      code: 0,
+      msg: 'no settle info'
+    }
+  }
+  if (settleInfo.confirmMembers.indexOf(openId) < 0) {
+    console.log('before ')
+    await db.collection('settle')
+      .doc(settleInfo._id)
+      .update({
+        data: {
+          confirmMembers: _.push(openId)
+        }
+      });
+  }
 
-  // return {
-  //   code: 1,
-  //   msg: 'confirm succss'
-  // }
+  return {
+    code: 1,
+    msg: 'confirm succss'
+  }
 }
